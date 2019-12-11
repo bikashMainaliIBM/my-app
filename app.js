99e7ef044edd24c145d133ff88a44ea367ed2f2c
Esther <43142802+ebaek@users.noreply.github.com>
2019-12-11 12:51:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("may work!!");
+  res.send("hello everyone! this was changed right before our presentation!!!!");
 });
  
 module.exports.app = app;

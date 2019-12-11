ff57824d145d8badaeb791c0e1c19d931515e08d
jackyng88 <jacky.ng9188@gmail.com>
2019-12-11 13:07:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("hello everyone! this was changed right before our presentation!!!! Hi Omar and Hamid");
+  res.send("hello everyone! this was changed right before our presentation!!!! Hi Omar, Hamid, Kirk, Matt and all.");
 });
  
 module.exports.app = app;
